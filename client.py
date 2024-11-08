a4f9b208a9f9116d9642b8a79da417fdf92b3af1
calinchan <145807733+calinchan@users.noreply.github.com>
2024-11-08 15:10:56 -0800

    Update client.py

--- client.py
+++ client.py
@@ -14,4 +14,24 @@ serv_sock.connect((host, port))
 print("Connected to server.")
 
 while True:
-  
+  print("\Available commands:")
+  print("JOIN <username> - Join server with username")
+  print("LIST - Get list of registered clients")
+  print("MESG <username> <mesg> - Send a message to a specific user")
+  print("BCST <message> - Broadcast a message to all registered clients")
+  print("QUIT - Leave the server")
+
+command = input("Enter command: ").strip()
+serv_sock.send(command.encode('ascii'))
+
+data = serv_sock.recv(1024)
+print('Received from server: ', data.decode('ascii'))
+
+if command.upper().startswith("QUIT"):
+  print("Disconnected from server.")
+  break
+
+serv_sock.close()
+
+if __name__ == '__main__':
+  main()
