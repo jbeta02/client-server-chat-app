2577aaf774519317ecb057afb42de4bb392e2c75
calinchan <145807733+calinchan@users.noreply.github.com>
2024-11-22 10:15:29 -0800

    Added comments

--- client.py
+++ client.py
@@ -1,3 +1,8 @@
+#Chloe Linchangco, Section 1, 11/22/2024
+#client.py is the client side that connects to the server and allows a user to join the chat room, send a list request to
+#see all registered users, broadcast a message to all users, send a private message to one registered user, or quit the
+#chatroom
+
 import sys
 import socket
 import threading
@@ -21,6 +26,7 @@ def main():
     port = int(sys.argv[2])
     serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    #If server can't connect, close the connection
     connection_status = serv_sock.connect_ex((host, port))
     if connection_status != 0:
         print("Unable to connect to the server")
@@ -28,6 +34,7 @@ def main():
     else:
         print("Connected to the server")
 
+    #Check to see if user has joined the chatroom
     joined = False
     username = ""
 
@@ -36,8 +43,10 @@ def main():
     listener_thread.daemon = True
     listener_thread.start()
 
+    #Let user join chatroom with username
     while True:
         if not joined:
+            #Ask user to input a username with JOIN command
             command = input("Enter JOIN followed by your username: ")
             if command.startswith("JOIN"):
                 username = command.split()[1]
@@ -47,11 +56,14 @@ def main():
                 if "joined" in response:
                     joined = True
             else:
+                #Prevent user from joining if JOIN command isn't used
                 print("You must join first with the command: JOIN <username>")
         else:
+            #Let user enter a command
             command = input("")
             serv_sock.send(command.encode('ascii'))
 
+            #User quits the chatroom and closes the server
             if command == "QUIT":
                 print(f"{username} is quitting the chat server")
                 serv_sock.close()
